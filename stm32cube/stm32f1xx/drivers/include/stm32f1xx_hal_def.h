6d23e88396ffa0afdd3094830bdb4e7b2d891fde
Peter Tönz <peter.tonz@husqvarnagroup.com>
2022-10-20 09:09:55 +0200

    defines: prevent redefinition of UNUSED
    To prevent compiler warnings/errors we have to wrap the definition of
    UNUSED with an ifndef. So there are no problems if this is already
    defined in the project
    
    Signed-off-by: Peter Tönz <peter.tonz@husqvarnagroup.com>

--- stm32cube/stm32f1xx/drivers/include/stm32f1xx_hal_def.h
+++ stm32cube/stm32f1xx/drivers/include/stm32f1xx_hal_def.h
@@ -65,7 +65,9 @@ typedef enum
                               (__DMA_HANDLE__).Parent = (__HANDLE__);             \
                           } while(0U)
 
-#define UNUSED(X) (void)X      /* To avoid gcc/g++ warnings */
+#ifndef UNUSED
+  #define UNUSED(X) (void)X      /* To avoid gcc/g++ warnings */
+#endif
 
 /** @brief Reset the Handle's State field.
   * @param __HANDLE__ specifies the Peripheral Handle.
