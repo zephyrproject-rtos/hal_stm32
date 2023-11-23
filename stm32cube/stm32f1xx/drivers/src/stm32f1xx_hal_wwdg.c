3c95fb1f819740e52c283abcd811c3e2a237f1bc
Abderrahmane Jarmouni <abderrahmane.jarmouni-ext@st.com>
2023-11-23 14:25:41 +0100

    stm32cube: update stm32f1 to cube version V1.8.5
    Update Cube version for STM32F1xx series
    on https://github.com/STMicroelectronics
    from version v1.8.4
    to version v1.8.5
    
    Signed-off-by: Abderrahmane Jarmouni <abderrahmane.jarmouni-ext@st.com>

--- stm32cube/stm32f1xx/drivers/src/stm32f1xx_hal_wwdg.c
+++ stm32cube/stm32f1xx/drivers/src/stm32f1xx_hal_wwdg.c
@@ -7,7 +7,18 @@
   *          functionalities of the Window Watchdog (WWDG) peripheral:
   *           + Initialization and de-initialization functions
   *           + IO operation functions
-  *           + Peripheral State functions
+  *	      + Peripheral State functions
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2016 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
   @verbatim
   ==============================================================================
                       ##### WWDG specific features #####
@@ -85,17 +96,6 @@
       (+) __HAL_WWDG_CLEAR_FLAG: Clear the WWDG's pending flags.
 
   @endverbatim
-  ******************************************************************************
-  * @attention
-  *
-  * <h2><center>&copy; Copyright (c) 2016 STMicroelectronics.
-  * All rights reserved.</center></h2>
-  *
-  * This software component is licensed by ST under BSD 3-Clause license,
-  * the "License"; You may not use this file except in compliance with the
-  * License. You may obtain a copy of the License at:
-  *                        opensource.org/licenses/BSD-3-Clause
-  *
   ******************************************************************************
   */
 
@@ -389,5 +389,3 @@ __weak void HAL_WWDG_EarlyWakeupCallback(WWDG_HandleTypeDef *hwwdg)
 /**
   * @}
   */
-
-/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
