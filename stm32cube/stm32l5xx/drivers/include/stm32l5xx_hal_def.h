38a49ae115b03ae37cb1d4fc5eba7ef1b0835af5
IBEN EL HADJ MESSAOUD Marwa <marwa.ibenelhadjmessaoud-ext@st.com>
2024-04-11 19:58:51 +0200

    stm32cube: update stm32l5 to cube version V1.5.1
    Update Cube version for STM32L5xx series
    on https://github.com/STMicroelectronics
    from version v1.5.0
    to version v1.5.1
    
    Signed-off-by: IBEN EL HADJ MESSAOUD Marwa <marwa.ibenelhadjmessaoud-ext@st.com>

--- stm32cube/stm32l5xx/drivers/include/stm32l5xx_hal_def.h
+++ stm32cube/stm32l5xx/drivers/include/stm32l5xx_hal_def.h
@@ -59,9 +59,9 @@ typedef enum
 
 /* Exported macros -----------------------------------------------------------*/
 
-#ifndef UNUSED
-  #define UNUSED(X) (void)X      /* To avoid gcc/g++ warnings */
-#endif
+#if !defined(UNUSED)
+#define UNUSED(X) (void)X      /* To avoid gcc/g++ warnings */
+#endif /* UNUSED */
 
 #define HAL_MAX_DELAY      0xFFFFFFFFU
 
