2b7bda2bdbdab64b71678553e86c707fc88e5fae
Guillaume Gautier <guillaume.gautier-ext@st.com>
2024-09-24 16:30:16 +0200

    stm32cube: update stm32u5 to cube version V1.6.0
    Update Cube version for STM32U5xx series
    on https://github.com/STMicroelectronics
    from version v1.5.0
    to version v1.6.0
    
    Signed-off-by: Guillaume Gautier <guillaume.gautier-ext@st.com>

--- stm32cube/stm32u5xx/soc/stm32u5g9xx.h
+++ stm32cube/stm32u5xx/soc/stm32u5g9xx.h
@@ -28343,7 +28343,7 @@ typedef struct
 #define USB_OTG_GCCFG_FSVPLUS_Msk                (0x1U << USB_OTG_GCCFG_FSVPLUS_Pos)          /*!< 0x00000002 */
 #define USB_OTG_GCCFG_FSVPLUS                    USB_OTG_GCCFG_FSVPLUS_Msk                    /*!< Single-Ended DP2 indicator DP voltage level  */
 #define USB_OTG_GCCFG_FSVMINUS_Pos               (2U)
-#define USB_OTG_GCCFG_FSVMINUS_Msk               0x1U << USB_OTG_GCCFG_FSVMINUS_Pos)         /*!< 0x00000004 */
+#define USB_OTG_GCCFG_FSVMINUS_Msk               (0x1U << USB_OTG_GCCFG_FSVMINUS_Pos)        /*!< 0x00000004 */
 #define USB_OTG_GCCFG_FSVMINUS                   USB_OTG_GCCFG_FSVMINUS_Msk                  /*!< Single-Ended DM2 indicator DM voltage level  */
 #define USB_OTG_GCCFG_SESSVLD_Pos                (3U)
 #define USB_OTG_GCCFG_SESSVLD_Msk                (0x1U << USB_OTG_GCCFG_SESSVLD_Pos)          /*!< 0x00000008 */
