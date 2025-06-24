991be07580018df27e553949958db530631da914
ExaltZephyr <ExaltZephyr@exalt.ps>
2025-06-24 15:47:32 +0300

    stm32cube: stm32h7: sdio: fix SDIO polling mode transfer issues
    this commit fixes two issues in SDIO driver:
    incorrect block count calculation and improper
    buffer size handling.
    
    Signed-off-by: Sarah Younis <zephyr@exalt.ps>

--- stm32cube/stm32h7xx/drivers/src/stm32h7xx_hal_sdio.c
+++ stm32cube/stm32h7xx/drivers/src/stm32h7xx_hal_sdio.c
@@ -1002,7 +1002,7 @@ HAL_StatusTypeDef HAL_SDIO_ReadExtended(SDIO_HandleTypeDef *hsdio, HAL_SDIO_Exte
     cmd |= Argument->Block_Mode << 27U;
     cmd |= Argument->OpCode << 26U;
     cmd |= (Argument->Reg_Addr & 0x1FFFFU) << 9U;
-    cmd |= (Size_byte & 0x1FFU);
+    cmd |= ((nbr_of_block == 0U)? Size_byte : nbr_of_block)& 0x1FFU;
     errorstate = SDMMC_SDIO_CmdReadWriteExtended(hsdio->Instance, cmd);
     if (errorstate != HAL_SDIO_ERROR_NONE)
     {
@@ -1147,7 +1147,7 @@ HAL_StatusTypeDef HAL_SDIO_WriteExtended(SDIO_HandleTypeDef *hsdio, HAL_SDIO_Ext
   uint8_t byteCount;
   uint32_t data;
   uint32_t dataremaining;
-  uint8_t *u32tempbuff = pData;
+  uint32_t *u32tempbuff = (uint32_t *) pData;
   uint32_t nbr_of_block;
 
   /* Check the parameters */
@@ -1207,7 +1207,7 @@ HAL_StatusTypeDef HAL_SDIO_WriteExtended(SDIO_HandleTypeDef *hsdio, HAL_SDIO_Ext
     cmd |= Argument->Block_Mode << 27U;
     cmd |= Argument->OpCode << 26U;
     cmd |= (Argument->Reg_Addr & 0x1FFFFU) << 9U;
-    cmd |= (Size_byte & 0x1FFU);
+    cmd |= ((nbr_of_block == 0U)? Size_byte : nbr_of_block)& 0x1FFU;
     errorstate = SDMMC_SDIO_CmdReadWriteExtended(hsdio->Instance, cmd);
     if (errorstate != HAL_SDIO_ERROR_NONE)
     {
