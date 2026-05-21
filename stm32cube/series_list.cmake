92574d13c26e8117c73c15d0b0b63d98ad5d09d1
Mathieu Choplain <mathieu.choplain-ext@st.com>
2026-05-21 18:28:31 +0200

    stm32cube: move series list from CMakeLists.txt to a dedicated file
    This may appear useless for now but will come in handy later as it makes
    automatic generation of the list almost trivial.
    
    Signed-off-by: Mathieu Choplain <mathieu.choplain-ext@st.com>

---
+++ stm32cube/series_list.cmake
@@ -0,0 +1,34 @@
+# SPDX-FileCopyrightText: Copyright (c) 2026 STMicroelectronics
+# SPDX-License-Identifier: Apache-2.0
+
+# See `CMakeLists.txt` for details.
+set(supported_series
+  stm32c0x
+  stm32c5x
+  stm32f0x
+  stm32f1x
+  stm32f2x
+  stm32f3x
+  stm32f4x
+  stm32f7x
+  stm32g0x
+  stm32g4x
+  stm32h5x
+  stm32h7x
+  stm32h7rsx
+  stm32l0x
+  stm32l1x
+  stm32l4x
+  stm32l5x
+  stm32mp1x
+  stm32mp13x
+  stm32mp2x
+  stm32n6x
+  stm32u0x
+  stm32u3x
+  stm32u5x
+  stm32wb0x
+  stm32wbax
+  stm32wbx
+  stm32wlx
+)
